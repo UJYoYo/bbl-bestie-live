fabc4a106c9f0fcccc99bf0acfbf33f69066afaf
UJYoYo <ujbaobao9899@gmail.com>
2025-08-24 00:15:36 +0700

    changed app for railway

--- app.py
+++ app.py
@@ -5,7 +5,41 @@ from backend.table import create_tables
 from backend.webhook_funcs import store_transaction_db
 from backend.jwt_utils import get_public_key, verify_jwt_payload
 
+from flask import Flask, request, abort
+
+
+from linebot.v3 import (
+    WebhookHandler
+)
+from linebot.v3.exceptions import (
+    InvalidSignatureError
+)
+from linebot.v3.messaging import (
+    Configuration,
+    ApiClient,
+    MessagingApi,
+    ReplyMessageRequest,
+    TextMessage
+)
+from linebot.v3.webhooks import (
+    MessageEvent,
+    TextMessageContent
+)
+
 app = Flask(__name__)
+
+# LINE credentials (add these)
+LINE_CHANNEL_ACCESS_TOKEN = os.environ.get('LINE_CHANNEL_ACCESS_TOKEN')
+if LINE_CHANNEL_ACCESS_TOKEN:
+    return "channel access token found"
+
+LINE_CHANNEL_SECRET = os.environ.get('LINE_CHANNEL_SECRET')
+if LINE_CHANNEL_SECRET:
+    print "secret found"
+
+configuration = Configuration(LINE_CHANNEL_ACCESS_TOKEN)
+handler = WebhookHandler(LINE_CHANNEL_SECRET)
+
 
 @app.route("/")
 def home():
@@ -16,7 +50,7 @@ def init_db():
         create_tables()
 
 
-@app.route("/webhook", methods=["POST"])
+@app.route("/webhook/bank", methods=["POST"])
 def receive_webhook():
     try:
         jwt_token = request.headers.get('X-JWT-Signature')
@@ -46,6 +80,35 @@ def receive_webhook():
         return f"Missing:{e}", 500
 
 
+@app.route("/webhook/line-oa", methods=['POST'])
+def callback():
+    # get X-Line-Signature header value
+    signature = request.headers['X-Line-Signature']
+
+    # get request body as text
+    body = request.get_data(as_text=True)
+    app.logger.info("Request body: " + body)
+
+    # handle webhook body
+    try:
+        handler.handle(body, signature)
+    except InvalidSignatureError:
+        app.logger.info("Invalid signature. Please check your channel access token/channel secret.")
+        abort(400)
+
+    return 'OK'
+
+
+@handler.add(MessageEvent, message=TextMessageContent)
+def handle_message(event):
+    with ApiClient(configuration) as api_client:
+        line_bot_api = MessagingApi(api_client)
+        line_bot_api.reply_message_with_http_info(
+            ReplyMessageRequest(
+                reply_token=event.reply_token,
+                messages=[TextMessage(text=event.message.text)]
+            )
+        )
 
 if __name__ == "__main__":
     init_db()
